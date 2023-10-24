ce4f4667fe78650522ab7bb3d4b393b78817c2db
Natalie Weires <nweires@google.com>
2023-10-24 17:44:03 +0000

    Respond to comments in to PR review

--- buildstockbatch/gcp/gcp.py
+++ buildstockbatch/gcp/gcp.py
@@ -23,19 +23,18 @@ import logging
 import os
 import pathlib
 import re
+import shutil
 
 from google.cloud import batch_v1
 
-from buildstockbatch.aws.aws import DockerBatchBase
-from buildstockbatch.base import ValidationError, BuildStockBatchBase
-from buildstockbatch import postprocessing
-from buildstockbatch.utils import ContainerRuntime, log_error_details, get_project_configuration
+from buildstockbatch.base import BuildStockBatchBase
+from buildstockbatch.utils import ContainerRuntime, log_error_details
 
 logger = logging.getLogger(__name__)
 
+
 # todo: aws-shared (see file comment)
 class DockerBatchBase(BuildStockBatchBase):
-
     CONTAINER_RUNTIME = ContainerRuntime.DOCKER
 
     def __init__(self, project_filename):
@@ -45,8 +44,12 @@ class DockerBatchBase(BuildStockBatchBase):
         try:
             self.docker_client.ping()
         except:  # noqa: E722 (allow bare except in this case because error can be a weird non-class Windows API error)
-            logger.error('The docker server did not respond, make sure Docker Desktop is started then retry.')
-            raise RuntimeError('The docker server did not respond, make sure Docker Desktop is started then retry.')
+            logger.error(
+                'The docker server did not respond, make sure Docker Desktop is started then retry.'
+            )
+            raise RuntimeError(
+                'The docker server did not respond, make sure Docker Desktop is started then retry.'
+            )
 
     @staticmethod
     def validate_project(project_file):
@@ -55,6 +58,7 @@ class DockerBatchBase(BuildStockBatchBase):
     @property
     def docker_image(self):
         return f"nrel/openstudio:{self.os_version}"
+
 
 class GcpBatch(DockerBatchBase):
     # https://patorjk.com/software/taag/#p=display&f=Santa%20Clara&t=BuildStockBatch%20%20%2F%20GCP
@@ -70,16 +74,18 @@ class GcpBatch(DockerBatchBase):
         super().__init__(project_filename)
 
         # TODO: how short does this really need to be? What characters are allowed?
-        self.job_identifier = re.sub('[^0-9a-zA-Z-]+', '_', self.cfg['gcp']['job_identifier'])[:20]
+        self.job_identifier = re.sub('[^0-9a-zA-Z-]+', '_', self.cfg['gcp']['job_identifier'])[:10]
 
         self.project_filename = project_filename
-        self.gcp_project = self.cfg['gcp']['gcp_project']
+        self.gcp_project = self.cfg['gcp']['project']
         self.region = self.cfg['gcp']['region']
         self.ar_repo = self.cfg['gcp']['artifact_registry']['repository']
-        self.bucket = self.cfg['gcp']['gcs']['bucket']
-        self.prefix = self.cfg['gcp']['gcs']['prefix']
+        self.gcs_bucket = self.cfg['gcp']['gcs']['bucket']
+        self.gcs_prefix = self.cfg['gcp']['gcs']['prefix']
+        self.use_spot = self.cfg['gcp']['use_spot']
 
-        # Add timestamp to job ID, since duplicates aren't allowed (even between finished and new jobs)
+        # Add timestamp to job ID, since duplicates aren't allowed
+        # (even between finished and new jobs)
         # TODO: stop appending timestamp here - it's useful for testing, but we should probably
         # make users choose a unique job ID each time.
         self.unique_job_id = self.job_identifier + datetime.utcnow().strftime('%y-%m-%d-%H%M%S')
@@ -91,7 +97,7 @@ class GcpBatch(DockerBatchBase):
 
     @staticmethod
     def validate_project(project_file):
-        return # todo-xxx- to be (re)implemented
+        return  # todo-xxx- to be (re)implemented
         super(GcpBatch, GcpBatch).validate_project(project_file)
         GcpBatch.validate_dask_settings(project_file)
         GcpBatch.validate_gcp_args(project_file)
@@ -142,7 +148,9 @@ class GcpBatch(DockerBatchBase):
             # Ensure the custom Gemfile exists in the buildstock dir
             local_gemfile_path = os.path.join(self.buildstock_dir, 'resources', 'Gemfile')
             if not os.path.exists(local_gemfile_path):
-                raise AttributeError(f'baseline:custom_gems = True, but did not find Gemfile at {local_gemfile_path}')
+                raise AttributeError(
+                    f'baseline:custom_gems = True, but did not find Gemfile at {local_gemfile_path}'
+                )
 
             # Copy the custom Gemfile into the buildstockbatch repo
             bsb_root = os.path.join(os.path.abspath(__file__), os.pardir, os.pardir, os.pardir)
@@ -165,7 +173,7 @@ class GcpBatch(DockerBatchBase):
             rm=True,
             target=stage,
             platform="linux/amd64",
-            buildargs={'OS_VER': self.os_version}
+            buildargs={'OS_VER': self.os_version},
         )
         build_image_log = os.path.join(local_log_dir, 'build_image.log')
         with open(build_image_log, 'w') as f_out:
@@ -182,10 +190,7 @@ class GcpBatch(DockerBatchBase):
         # Report and confirm the openstudio version from the image
         os_ver_cmd = 'openstudio openstudio_version'
         container_output = self.docker_client.containers.run(
-            self.docker_image,
-            os_ver_cmd,
-            remove=True,
-            name='list_openstudio_version'
+            self.docker_image, os_ver_cmd, remove=True, name='list_openstudio_version'
         )
         assert self.os_version in container_output.decode()
 
@@ -193,13 +198,12 @@ class GcpBatch(DockerBatchBase):
         # The OpenStudio Docker image installs the default gems
         # to /var/oscli/gems, and the custom docker image
         # overwrites these with the custom gems.
-        list_gems_cmd = 'openstudio --bundle /var/oscli/Gemfile --bundle_path /var/oscli/gems ' \
-                        '--bundle_without native_ext gem_list'
+        list_gems_cmd = (
+            'openstudio --bundle /var/oscli/Gemfile --bundle_path /var/oscli/gems '
+            '--bundle_without native_ext gem_list'
+        )
         container_output = self.docker_client.containers.run(
-            self.docker_image,
-            list_gems_cmd,
-            remove=True,
-            name='list_gems'
+            self.docker_image, list_gems_cmd, remove=True, name='list_gems'
         )
         gem_list_log = os.path.join(local_log_dir, 'openstudio_gem_list_output.log')
         with open(gem_list_log, 'wb') as f_out:
@@ -217,9 +221,7 @@ class GcpBatch(DockerBatchBase):
         service_account_key_file = open(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], "r")
         service_account_key = service_account_key_file.read()
         docker_client_login_response = self.docker_client.login(
-            username="_json_key",
-            password=service_account_key,
-            registry=self.registry_url
+            username="_json_key", password=service_account_key, registry=self.registry_url
         )
         logger.debug(docker_client_login_response)
 
@@ -240,11 +242,9 @@ class GcpBatch(DockerBatchBase):
                     logger.debug(y['status'])
                     last_status = y['status']
 
-
-    def cleanup(self):
+    def clean(self):
         # TODO: clean up all resources used for this project
         pass
-
 
     def list_jobs(self):
         """
@@ -281,7 +281,8 @@ class GcpBatch(DockerBatchBase):
         # buildstock_csv_filename = self.sampler.run_sampling()
 
         # TODO: Step 2: Upload weather data and any other required files to GCP
-        # TODO: split samples into smaller batches to be run by individual tasks (reuse logic from aws.py)
+        # TODO: split samples into smaller batches to be run by individual tasks
+        # (reuse logic from aws.py)
 
         # Step 3: Define and run the GCP Batch job.
         client = batch_v1.BatchServiceClient()
@@ -289,7 +290,9 @@ class GcpBatch(DockerBatchBase):
         runnable = batch_v1.Runnable()
         runnable.container = batch_v1.Runnable.Container()
         # TODO: Use the docker image pushed earlier instead of this hardcoded image.
-        runnable.container.image_uri = 'us-central1-docker.pkg.dev/buildstockbatch-dev/buildstockbatch-docker/buildstockbatch'
+        runnable.container.image_uri = (
+            'us-central1-docker.pkg.dev/buildstockbatch-dev/buildstockbatch-docker/buildstockbatch'
+        )
         runnable.container.entrypoint = '/bin/sh'
 
         # Pass environment variables to each task
@@ -299,53 +302,59 @@ class GcpBatch(DockerBatchBase):
         environment.variables = {'JOB_ID': self.unique_job_id}
         runnable.environment = environment
 
-        # TODO: Update to run batches of openstudio with something like "python3 -m buildstockbatch.gcp.gcp"
+        # TODO: Update to run batches of openstudio with something like
+        # "python3 -m buildstockbatch.gcp.gcp"
         runnable.container.commands = [
             "-c",
             # Test script that checks whether openstudio is installed and writes to a file in GCS.
-            "mkdir /mnt/disks/share/${JOB_ID}; openstudio --help > /mnt/disks/share/${JOB_ID}/output_${BATCH_TASK_INDEX}.txt"
+            "mkdir /mnt/disks/share/${JOB_ID}; "
+            "openstudio --help > /mnt/disks/share/${JOB_ID}/output_${BATCH_TASK_INDEX}.txt",
         ]
 
         # Mount GCS Bucket, so we can use it like a normal directory.
-        gcs_bucket = batch_v1.GCS(remote_path=self.bucket)
+        gcs_bucket = batch_v1.GCS(remote_path=self.gcs_bucket)
         # Note: 'mnt/share/' is read-only, but 'mnt/disks/share' works
         gcs_volume = batch_v1.Volume(
-            gcs = gcs_bucket,
-            mount_path = '/mnt/disks/share',
+            gcs=gcs_bucket,
+            mount_path='/mnt/disks/share',
         )
 
         # TODO: Allow specifying resources from the project YAML file, plus pick better defaults.
         resources = batch_v1.ComputeResource(
-            cpu_milli = 1000,
-            memory_mib = 1,
+            cpu_milli=1000,
+            memory_mib=1,
         )
 
         task = batch_v1.TaskSpec(
-            runnables = [runnable],
-            volumes = [gcs_volume],
-            compute_resource = resources,
-            # TODO: Confirm what happens if this fails repeatedly, or for only some tasks, and document it.
-            max_retry_count = 2,
+            runnables=[runnable],
+            volumes=[gcs_volume],
+            compute_resource=resources,
+            # TODO: Confirm what happens if this fails repeatedly, or for only
+            # some tasks, and document it.
+            max_retry_count=2,
             # TODO: How long does this timeout need to be?
-            max_run_duration = '60s'
+            max_run_duration='60s',
         )
 
         # How many of these tasks to run.
         # TODO: determine this count above, when splitting up samples.
         task_count = 3
         group = batch_v1.TaskGroup(
-            task_count = task_count,
-            task_spec = task,
+            task_count=task_count,
+            task_spec=task,
         )
 
         # Specify types of VMs to run on
         # TODO: look into best default machine type for running OpenStudio, but also allow
         # changing via the project config. https://cloud.google.com/compute/docs/machine-types
         policy = batch_v1.AllocationPolicy.InstancePolicy(
-            machine_type = 'e2-standard-2',
+            machine_type='e2-standard-2',
         )
         instances = batch_v1.AllocationPolicy.InstancePolicyOrTemplate(policy=policy)
-        allocation_policy = batch_v1.AllocationPolicy(instances = [instances])
+        allocation_policy = batch_v1.AllocationPolicy(
+            instances=[instances],
+            # TODO: Support using Spot instances
+        )
         # TODO: Add option to set service account that runs the job?
         # Otherwise uses the project's default compute engine service account.
         # allocation_policy.service_account = batch_v1.ServiceAccount(email = '')
@@ -361,14 +370,16 @@ class GcpBatch(DockerBatchBase):
 
         # Send notifications to pub/sub when the job's state changes
         # TODO: Turn these into emails if an email address is provided?
-        job.notifications = [batch_v1.JobNotification(
-            # TODO: Get topic from config or create a new one as needed (via terraform)
-            pubsub_topic = 'projects/buildstockbatch-dev/topics/notifications',
-            message = batch_v1.JobNotification.Message(
-                type_ = 1,
-                new_job_state='STATE_UNSPECIFIED', # notify on any changes(?)
+        job.notifications = [
+            batch_v1.JobNotification(
+                # TODO: Get topic from config or create a new one as needed (via terraform)
+                pubsub_topic='projects/buildstockbatch-dev/topics/notifications',
+                message=batch_v1.JobNotification.Message(
+                    type_=1,
+                    new_job_state='STATE_UNSPECIFIED',  # notify on any changes
+                ),
             )
-        )]
+        ]
 
         create_request = batch_v1.CreateJobRequest()
         create_request.job = job
@@ -383,9 +394,9 @@ class GcpBatch(DockerBatchBase):
         logger.info(f'Job name: {created_job.name}')
         logger.info(f'Job UID: {created_job.uid}')
 
-
     @classmethod
-    def run_job(self, job_id, bucket, prefix, job_name, region): """
+    def run_job(self, job_id, gcs_bucket, gcs_prefix, job_name, region):
+        """
         Run a few simulations inside a container.
 
         This method is called from inside docker container in GCP compute engine.
@@ -397,40 +408,33 @@ class GcpBatch(DockerBatchBase):
 
 @log_error_details()
 def main():
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': True,
-        'formatters': {
-            'defaultfmt': {
-                'format': '%(levelname)s:%(asctime)s:%(name)s:%(message)s',
-                'datefmt': '%Y-%m-%d %H:%M:%S'
-            }
-        },
-        'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'defaultfmt',
-                'level': 'DEBUG',
-                'stream': 'ext://sys.stdout',
-            }
-        },
-        'loggers': {
-            '__main__': {
-                'level': 'DEBUG',
-                'propagate': True,
-                'handlers': ['console']
+    logging.config.dictConfig(
+        {
+            'version': 1,
+            'disable_existing_loggers': True,
+            'formatters': {
+                'defaultfmt': {
+                    'format': '%(levelname)s:%(asctime)s:%(name)s:%(message)s',
+                    'datefmt': '%Y-%m-%d %H:%M:%S',
+                }
             },
-            'buildstockbatch': {
-                'level': 'DEBUG',
-                'propagate': True,
-                'handlers': ['console']
+            'handlers': {
+                'console': {
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'defaultfmt',
+                    'level': 'DEBUG',
+                    'stream': 'ext://sys.stdout',
+                }
             },
-        },
-    })
+            'loggers': {
+                '__main__': {'level': 'DEBUG', 'propagate': True, 'handlers': ['console']},
+                'buildstockbatch': {'level': 'DEBUG', 'propagate': True, 'handlers': ['console']},
+            },
+        }
+    )
     print(GcpBatch.LOGO)
     if 'BATCH_TASK_INDEX' in os.environ:
         # If this var exists, we're inside a single batch task.
-        # TODO: Would it be cleaner to move this to a main() in another file?
         job_id = int(os.environ['BATCH_TASK_INDEX'])
         # TODO: pass in these env vars to tasks as needed. (Do we really need bucket here?)
         gcs_bucket = os.environ['GCS_BUCKET']
@@ -443,7 +447,8 @@ def main():
         parser.add_argument('project_filename')
         group = parser.add_mutually_exclusive_group()
         group.add_argument(
-            '-c', '--clean',
+            '-c',
+            '--clean',
             action='store_true',
             help='After the simulation is done, run with --clean to clean up GCP environment'
             # TODO: Clarify whether this will also stop a job that's still running.
@@ -451,27 +456,24 @@ def main():
         group.add_argument(
             '--validateonly',
             help='Only validate the project YAML file and references. Nothing is executed',
-            action='store_true'
-        )
-        parser.add_argument(
-            '--list_jobs',
-            help='List existing jobs',
-            action='store_true'
-        )
-        parser.add_argument(
-            '-v', '--verbose',
             action='store_true',
-            help='Verbose output'
+        )
+        parser.add_argument('--list_jobs', help='List existing jobs', action='store_true')
+        parser.add_argument(
+            '-v',
+            '--verbose',
+            action='store_true',
+            help='Verbose output - includes DEBUG logs if set',
         )
         group.add_argument(
             '--postprocessonly',
             help='Only do postprocessing, useful for when the simulations are already done',
-            action='store_true'
+            action='store_true',
         )
         group.add_argument(
             '--crawl',
             help='Only do the crawling in Athena. When simulations and postprocessing are done.',
-            action='store_true'
+            action='store_true',
         )
         args = parser.parse_args()
 
@@ -480,8 +482,7 @@ def main():
         else:
             logger.setLevel(logging.INFO)
 
-
-        # validate the project, and in case of the --validateonly flag return True if validation passes
+        # validate the project, and if --validateonly flag is set, return True if validation passes
         GcpBatch.validate_project(os.path.abspath(args.project_filename))
         if args.validateonly:
             return True
@@ -493,7 +494,7 @@ def main():
             # Note: cleanup also requires the project input file, and only should only clean
             # up resources from that particular project.
             # TODO: should this also stop the job if it's currently running?
-            batch.cleanup()
+            batch.clean()
             return
         if args.list_jobs:
             batch.list_jobs()
