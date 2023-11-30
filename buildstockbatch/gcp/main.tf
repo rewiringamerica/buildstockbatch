dbe936a4f67f34a9925e99ce2075e8dc59fc67ce
Natalie Weires <nweires@google.com>
2023-11-30 17:55:48 +0000

    Add beta provider

--- buildstockbatch/gcp/main.tf
+++ buildstockbatch/gcp/main.tf
@@ -46,6 +46,11 @@ provider "google" {
   region  = var.region
 }
 
+provider "google-beta" {
+  project = var.gcp_project
+  region  = var.region
+}
+
 # GCS bucket for storing inputs and outputs
 resource "google_storage_bucket" "bucket" {
   name                        = var.bucket_name
