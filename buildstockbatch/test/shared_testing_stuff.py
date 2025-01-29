9eb51bb5ed4c533308d828053971d5c75881292c
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2025-01-29 10:28:20 -0600

    Revert back test changes - remove sdr yaml

--- buildstockbatch/test/shared_testing_stuff.py
+++ buildstockbatch/test/shared_testing_stuff.py
@@ -8,7 +8,7 @@ import pytest
 resstock_directory = pathlib.Path(
     os.environ.get(
         "RESSTOCK_DIR",
-        pathlib.Path(__file__).resolve().parent.parent.parent.parent / "resstock",
+        pathlib.Path(__file__).resolve().parent.parent.parent.parent / "resstock_loadflex",
     )
 )
 resstock_required = pytest.mark.skipif(not resstock_directory.exists(), reason="ResStock checkout is not found")
