c5a6ffc4c7ef8441e20aead1fb09873ab0f4c4c4
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2025-01-29 16:58:06 -0600

    Rename to project_directory

--- buildstockbatch/workflow_generator/commercial/latest/__init__.py
+++ buildstockbatch/workflow_generator/commercial/latest/__init__.py
@@ -2,7 +2,7 @@ __version__ = "2025.01.29"
 version_info = {
     "version": __version__,
     "version_description": """
-In this version, the BuildExistingModel and ApplyUpgrade measures are passed project_name argument.
+In this version, the BuildExistingModel and ApplyUpgrade measures are passed project_directory argument.
 In previous versions, we were copying housing_characteristics and measures into lib folder.
 """,
 }
